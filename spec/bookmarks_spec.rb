695593f752a3d4e1268bffcdd3e269c1bf7a0396
Borislav Lazarov <lazarov_borislav@yahoo.com>
2021-09-14 16:44:01 +0100

    Interacting with PostgreSQL from Ruby

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -5,8 +5,8 @@ describe Bookmark do
         it 'returns all bookmarks' do
             bookmarks = Bookmark.all
             expect(bookmarks).to include("www.ruby.com")  
-            expect(bookmarks).to include("www.yahoo.com")
             expect(bookmarks).to include("www.google.com")
+            expect(bookmarks).to include("www.makersk.com")
         end
     end
 end
--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -3,8 +3,8 @@ feature 'Viewing bookmarks' do
     scenario 'visiting the index page' do
       visit('/bookmarks')
       expect(page).to have_content "www.ruby.com" 
-      expect(page).to have_content "www.yahoo.com"
       expect(page).to have_content "www.google.com"
+      expect(page).to have_content "www.makersk.com"
     end
   end
   
